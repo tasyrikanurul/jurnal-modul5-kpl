75cf374f0610102f5669f1d9d554bb267ac0a2c1
tasyrikanurul <tasyrikanurul@student.telkomuniversity.ac.id>
2022-03-26 14:27:11 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ modul5_1302201131/SayaTubeUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302201131
+{
+    internal class SayaTubeUser
+    {
+        private int id;
+        private List<SayaTubeVideo> uploadedVideos;
+        public string Username;
+
+        public string tubeuser;
+        private object playCount;
+
+        public SayaTubeUser(string tubeuser)
+        {
+            this.tubeuser = tubeuser;
+        }
+
+       
+        public GetTotalVideoPlayCount()
+        {
+            return playCount;
+        }
+            
+        
+    }
+}
